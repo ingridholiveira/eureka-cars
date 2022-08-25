24384964926300851819d2e709a7ccc62d6ae19d
Ingridh Oliveira <ingridhnunes@gmail.com>
2022-08-25 11:50:18 -0300

    Adicionando bootstrap e criação de componente navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import "./assets/styles/custom.scss";
 import "./App.css";
+import Navbar from "components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <h1>Carros Topíssimos</h1>
     </>
   );
