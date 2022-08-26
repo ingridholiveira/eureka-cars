d28376322079826e1849b2ddf05002ea82e5760d
Ingridh Oliveira <ingridhnunes@gmail.com>
2022-08-26 11:40:28 -0300

    estlizando navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,15 +1,33 @@
+import "./styles.css";
+import "bootstrap/js/src/collapse.js";
+
 const Navbar = () => {
   return (
     <>
-      <nav className="bg-primary">
-        <div>
-          <a href="link">
-            <h4>DS Catalog</h4>
+      <nav className="bg-primary navbar navbar-expand-md main-nav">
+        <div className="container-fluid">
+          <a href="link" className="nav-logo-text">
+            <h4>Carros Top</h4>
           </a>
-          <div>
-            <ul>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#dscarros-navbar"
+            aria-controls="dscarros-navbar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="dscarros-navbar">
+            <ul className="navbar-nav offset-md-10 main-menu">
               <li>
-                <a href="link">Home</a>
+                <a href="link" className="active">
+                  Home
+                </a>
               </li>
               <li>
                 <a href="link">Catálogo</a>
